19b1778d5fa9b3548a99b5d7b1f13da56debe702
aya-215 <aya.chr928@gmail.com>
2025-11-20 16:55:55 +0900

    feat: nvim-tree設定を拡張してカレントディレクトリ追従機能を追加
    nvim-treeの動作を改善：
    - sync_root_with_cwd: カレントディレクトリと同期
    - respect_buf_cwd: バッファのカレントディレクトリを尊重
    - update_focused_file: フォーカスファイルに応じてツリーのルートを更新

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -22,6 +22,12 @@ return {
         filters = {
           dotfiles = true,
         },
+        sync_root_with_cwd = true,
+        respect_buf_cwd = true,
+        update_focused_file = {
+          enable = true,
+          update_root = true,
+        },
       })
     end,
   },
